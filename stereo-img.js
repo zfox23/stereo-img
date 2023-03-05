27d8159d16184b7887909e4a70b1295f89dcf890
Zach Fox <valefox23@gmail.com>
2023-03-05 01:03:28 -0500

    Reformatted the code and made some updates that suit my projects - so grateful for this code

--- stereo-img.js
+++ stereo-img.js
@@ -18,156 +18,199 @@ import { parseAnaglyph } from './parsers/anaglyph-parser/anaglyph-parser.js';
 import exifr from 'exifr';
 
 import * as THREE from 'three';
-import { VRButton } from 'three/examples/jsm/webxr/VRButton.js';
-import { OrbitControls  } from 'three/examples/jsm/controls/OrbitControls.js';
+import { VRButton } from './vr-button.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 
 class StereoImg extends HTMLElement {
 
-    get type() {
-      return this.getAttribute('type');
+  get type() {
+    return this.getAttribute('type');
+  }
+  set type(val) {
+    if (val) {
+      this.setAttribute('type', val);
+    } else {
+      this.removeAttribute('type');
     }
-    set type(val) {
-      if (val) {
-        this.setAttribute('type', val);
+  }
+
+  get angle() {
+    return this.getAttribute('angle');
+  }
+  set angle(val) {
+    if (val) {
+      this.setAttribute('angle', val);
+    } else {
+      this.removeAttribute('angle');
+    }
+  }
+
+  get src() {
+    return this.getAttribute('src');
+  }
+  set src(val) {
+    if (val) {
+      this.setAttribute('src', val);
+    } else {
+      this.removeAttribute('src');
+    }
+
+    // use setTimeout to ensure all DOM updates have finished, it's indeed common to update both src= and type= at the same time.
+    // There is probably a cleaner way to do this.
+    let that = this;
+    window.setTimeout(() => {
+      that.parseImageAndInitialize3DScene();
+    }, 0);
+  }
+
+  get backImageSrc() {
+    return this.getAttribute('backImageSrc');
+  }
+  set backImageSrc(val) {
+    if (val) {
+      this.setAttribute('backImageSrc', val);
+    } else {
+      this.removeAttribute('backImageSrc');
+    }
+
+    // use setTimeout to ensure all DOM updates have finished, it's indeed common to update both src= and type= at the same time.
+    // There is probably a cleaner way to do this.
+    let that = this;
+    window.setTimeout(() => {
+      that.parseImageAndInitialize3DScene();
+    }, 0);
+  }
+
+  animate() {
+    this.renderer.setAnimationLoop(() => {
+      this.controls?.update();
+      this.renderer.render(this.scene, this.camera);
+    });
+  }
+
+  async parse() {
+    if (this.src) {
+      if (this.type === 'vr') {
+        this.stereoData = await parseVR(this.src);
+      } else if (this.type === 'left-right' || this.type === 'top-bottom') {
+        this.stereoData = await parseStereo(this.src, {
+          type: this.type,
+          angle: this.angle,
+        });
+      } else if (this.type === 'anaglyph') {
+        this.stereoData = await parseAnaglyph(this.src, {
+          angle: this.angle,
+        });
       } else {
-        this.removeAttribute('type');
-      }
-    }
+        // Read XMP metadata
+        const exif = await exifr.parse(this.src, {
+          xmp: true,
+          multiSegment: true,
+          mergeOutput: false,
+          ihdr: true, //unclear why we need this, but if not enabled, some VR180 XMP Data are not parsed
+        });
 
-    get angle() {
-      return this.getAttribute('angle');
-    }
-    set angle(val) {
-      if (val) {
-        this.setAttribute('angle', val);
-      } else {
-        this.removeAttribute('angle');
-      }
-    }
-
-    get src() {
-      return this.getAttribute('src');
-    }
-    set src(val) {
-      if (val) {
-        this.setAttribute('src', val);
-      } else {
-        this.removeAttribute('src');
-      }
-
-      // use setTimeout to ensure all DOM updates have finished, it's indeed common to update both src= and type= at the same time.
-      // There is probably a cleaner way to do this.
-      let that = this;
-      window.setTimeout(() => {
-        that.parseImageAndInitialize3DScene();
-      }, 0);
-    }
-
-    animate() {
-      this.renderer.setAnimationLoop( () => {
-        this.renderer.render( this.scene, this.camera );
-      } );
-    }
-  
-    async parse() {
-      if(this.src) {
-        if(this.type === 'vr') {
+        if (exif?.GImage?.Data) {
+          // XMP for left eye found, assume VR Photo
           this.stereoData = await parseVR(this.src);
-        } else if(this.type === 'left-right' || this.type === 'top-bottom') {
-          this.stereoData = await parseStereo(this.src, {
-            type: this.type,
-            angle: this.angle,
-          });
-        } else if(this.type === 'anaglyph') {
-          this.stereoData = await parseAnaglyph(this.src, {
-            angle: this.angle,
-          });
         } else {
-          // Read XMP metadata
-          const exif = await exifr.parse(this.src, {
-            xmp: true,
-            multiSegment: true,
-            mergeOutput: false,
-            ihdr: true, //unclear why we need this, but if not enabled, some VR180 XMP Data are not parsed
+          // no left eye found, assume left-right
+          console.warn('<stereo-img> does not have a "type" attribute and image does not have XMP metadata of a VR picture.  Use "type" attribute to specify the type of stereoscopic image. Assuming left-right stereo image.');
+          this.stereoData = await parseStereo(this.src, {
+            angle: this.angle,
           });
-
-          if (exif?.GImage?.Data) {
-            // XMP for left eye found, assume VR Photo
-            this.stereoData = await parseVR(this.src);
-          } else {
-            // no left eye found, assume left-right
-            console.warn('<stereo-img> does not have a "type" attribute and image does not have XMP metadata of a VR picture.  Use "type" attribute to specify the type of stereoscopic image. Assuming left-right stereo image.');
-            this.stereoData = await parseStereo(this.src, {
-              angle: this.angle,
-            });
-          }
         }
-      } else {
-        // no src attribute. Use fake stereo data.
-        this.stereoData = {
-          leftEye: new ImageData(10, 10),
-          rightEye: new ImageData(10, 10),
-          phiLength: 0,
-          thetaStart: 0,
-          thetaLength: 0
-        };
       }
+    } else {
+      // no src attribute. Use fake stereo data.
+      this.stereoData = {
+        leftEye: new ImageData(10, 10),
+        rightEye: new ImageData(10, 10),
+        phiLength: 0,
+        thetaStart: 0,
+        thetaLength: 0
+      };
     }
+  }
 
-    initialize3DScene() {
-      this.scene = new THREE.Scene();
-      this.scene.background = new THREE.Color( 0x101010 );
+  async initialize3DScene() {
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0x101010);
 
-      const radius = 10; // 500
+    const radius = 10; // 500
 
-			// left eye
-      const texture1 = new THREE.Texture(this.stereoData.leftEye);
-      texture1.needsUpdate = true;
+    // left eye
+    const texture1 = new THREE.Texture(this.stereoData.leftEye);
+    texture1.needsUpdate = true;
 
-      // TODO: Screen size should depend on image aspect ratio, camera fov...
-      const geometry1 = new THREE.SphereGeometry( radius, 60, 40, -1 * this.stereoData.phiLength / 2, this.stereoData.phiLength, this.stereoData.thetaStart, this.stereoData.thetaLength);
-      // invert the geometry on the x-axis so that all of the faces point inward
-      geometry1.scale( - 1, 1, 1 );
+    // TODO: Screen size should depend on image aspect ratio, camera fov...
+    const geometry1 = new THREE.SphereGeometry(radius, 60, 40, -1 * this.stereoData.phiLength / 2, this.stereoData.phiLength, this.stereoData.thetaStart, this.stereoData.thetaLength);
+    // invert the geometry on the x-axis so that all of the faces point inward
+    geometry1.scale(- 1, 1, 1);
 
-      const material1 = new THREE.MeshBasicMaterial( { map: texture1 } );
+    const material1 = new THREE.MeshBasicMaterial({ map: texture1 });
 
-      const mesh1 = new THREE.Mesh( geometry1, material1 );
-      mesh1.rotation.reorder( 'YXZ' );
-      mesh1.rotation.y = Math.PI / 2;
-      mesh1.rotation.x = this.stereoData.roll || 0;
-      mesh1.rotation.z = this.stereoData.pitch || 0;
-      mesh1.layers.set( 1 ); // display in left eye only
-      this.scene.add( mesh1 );
+    const mesh1 = new THREE.Mesh(geometry1, material1);
+    mesh1.rotation.reorder('YXZ');
+    mesh1.rotation.y = Math.PI / 2;
+    mesh1.rotation.x = this.stereoData.roll || 0;
+    mesh1.rotation.z = this.stereoData.pitch || 0;
+    mesh1.layers.set(1); // display in left eye only
+    this.scene.add(mesh1);
 
 
-      // right eye
+    // right eye
 
-      const texture2 = new THREE.Texture(this.stereoData.rightEye);
-      texture2.needsUpdate = true;
+    const texture2 = new THREE.Texture(this.stereoData.rightEye);
+    texture2.needsUpdate = true;
 
-      const geometry2 = new THREE.SphereGeometry( radius, 60, 40, -1 * this.stereoData.phiLength / 2, this.stereoData.phiLength, this.stereoData.thetaStart, this.stereoData.thetaLength);
-      geometry2.scale( - 1, 1, 1 );
+    const geometry2 = new THREE.SphereGeometry(radius, 60, 40, -1 * this.stereoData.phiLength / 2, this.stereoData.phiLength, this.stereoData.thetaStart, this.stereoData.thetaLength);
+    geometry2.scale(- 1, 1, 1);
 
-      const material2 = new THREE.MeshBasicMaterial( { map: texture2 } );
+    const material2 = new THREE.MeshBasicMaterial({ map: texture2 });
 
-      const mesh2 = new THREE.Mesh( geometry2, material2 );
-      mesh2.rotation.reorder( 'YXZ' );
-      mesh2.rotation.y = Math.PI / 2;
-      mesh2.rotation.x = this.stereoData.roll || 0;
-      mesh2.rotation.z = this.stereoData.pitch || 0;
-      mesh2.layers.set( 2 ); // display in right eye only
-      this.scene.add( mesh2 );
+    const mesh2 = new THREE.Mesh(geometry2, material2);
+    mesh2.rotation.reorder('YXZ');
+    mesh2.rotation.y = Math.PI / 2;
+    mesh2.rotation.x = this.stereoData.roll || 0;
+    mesh2.rotation.z = this.stereoData.pitch || 0;
+    mesh2.layers.set(2); // display in right eye only
+    this.scene.add(mesh2);
+
+
+    // "curious human" image
+    if (this.angle === "180" && this.backImageSrc) {
+      const backImage = new Image();
+      const texture3 = new THREE.Texture(backImage);
+      backImage.addEventListener('load', () => {
+        texture3.needsUpdate = true
+      });
+      backImage.src = this.backImageSrc;
+
+      const geometry3 = new THREE.SphereGeometry(radius, 60, 40, -1 * this.stereoData.phiLength / 2, this.stereoData.phiLength, this.stereoData.thetaStart, this.stereoData.thetaLength);
+      geometry3.scale(- 1, 1, 1);
+
+      const material3 = new THREE.MeshBasicMaterial({ map: texture3 });
+
+      const mesh3 = new THREE.Mesh(geometry3, material3);
+      mesh3.rotation.reorder('YXZ');
+      mesh3.rotation.y = -Math.PI / 2;
+      mesh3.rotation.x = this.stereoData.roll || 0;
+      mesh3.rotation.z = this.stereoData.pitch || 0;
+      mesh3.layers.set(0);
+      this.scene.add(mesh3);
     }
+  }
 
-    async parseImageAndInitialize3DScene() {
-      await this.parse();
-      this.initialize3DScene();
-    }
+  async parseImageAndInitialize3DScene() {
+    await this.parse();
+    await this.initialize3DScene();
+  }
 
-    async init() {
-      this.attachShadow({mode: 'open'});
-      this.shadowRoot.innerHTML = `
+  async init() {
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = `
       <style>
         :host {
           display: block;
@@ -176,49 +219,55 @@ class StereoImg extends HTMLElement {
       </style>
       `;
 
-      // TODO: should we also read width and height attributes and resize element accordingly?
-      if(this.clientHeight === 0) {
-        const aspectRatio = 4 / 3;
-        this.style.height = this.clientWidth / aspectRatio + "px";
-      }
+    // TODO: should we also read width and height attributes and resize element accordingly?
+    if (this.clientHeight === 0) {
+      const aspectRatio = 4 / 3;
+      this.style.height = this.clientWidth / aspectRatio + "px";
+    }
 
-      await this.parseImageAndInitialize3DScene();
+    await this.parseImageAndInitialize3DScene();
 
-      this.renderer = new THREE.WebGLRenderer();
-      this.renderer.setPixelRatio(window.devicePixelRatio);
-      this.renderer.xr.enabled = true;
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.xr.enabled = true;
+    this.renderer.setSize(this.clientWidth, this.clientHeight);
+    this.shadowRoot.appendChild(this.renderer.domElement);
+
+    // TODO: Should we use component size instead?
+    this.camera = new THREE.PerspectiveCamera(70, this.clientWidth / this.clientHeight, 1, 2000);
+    this.camera.layers.enable(1);
+
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.enableDamping = true;
+    this.controls.dampingFactor = 0.20;
+    this.controls.rotateSpeed = -0.25;
+    this.controls.enableZoom = false;
+    this.controls.addEventListener("change", () => {
+      if (this.renderer) this.renderer.render(this.scene, this.camera);
+    });
+    this.camera.position.set(0, 0, 0.1);
+
+    this.shadowRoot.appendChild(VRButton.createButton(this.renderer));
+
+    this.animate();
+
+    // Listen for component resize
+    const resizeObserver = new ResizeObserver(() => {
       this.renderer.setSize(this.clientWidth, this.clientHeight);
-      this.shadowRoot.appendChild(this.renderer.domElement);
+      this.camera.aspect = this.clientWidth / this.clientHeight;
+      this.camera.updateProjectionMatrix();
+    });
 
-      // TODO: Should we use component size instead?
-      this.camera = new THREE.PerspectiveCamera( 70, this.clientWidth / this.clientHeight, 1, 2000 );
-      this.camera.layers.enable( 1 );
-
-      const controls = new OrbitControls( this.camera, this.renderer.domElement );
-      this.camera.position.set(0, 0, 0.1);
-      controls.update();
-
-      this.shadowRoot.appendChild(VRButton.createButton(this.renderer));
-
-      this.animate();
-
-      // Listen for component resize
-      const resizeObserver = new ResizeObserver(() => {
-        this.renderer.setSize(this.clientWidth, this.clientHeight);
-        this.camera.aspect = this.clientWidth / this.clientHeight;
-        this.camera.updateProjectionMatrix();
-      });
-
-      resizeObserver.observe(this);
-    }
-
-
-    constructor() {
-      super();
-      this.init();
-    }
-
+    resizeObserver.observe(this);
   }
+
+
+  constructor() {
+    super();
+    this.init();
+  }
+
+}
 
 window.customElements.define('stereo-img', StereoImg);
 
---
+++ vr-button.js
@@ -0,0 +1,143 @@
+class VRButton {
+    static createButton(renderer) {
+        const button = document.createElement('button');
+
+        function showEnterVR( /*device*/) {
+            let currentSession = null;
+
+            async function onSessionStarted(session) {
+                session.addEventListener('end', onSessionEnded);
+                await renderer.xr.setSession(session);
+                button.textContent = 'Exit VR';
+                currentSession = session;
+            }
+
+            function onSessionEnded( /*event*/) {
+                currentSession.removeEventListener('end', onSessionEnded);
+                button.textContent = 'View in VR';
+                currentSession = null;
+
+            }
+            
+            button.style.display = '';
+            button.style.cursor = 'pointer';
+            button.style.left = 'calc(50% - 50px)';
+            button.style.width = '120px';
+
+            button.textContent = 'View in VR';
+
+            button.onclick = () => {
+                if (currentSession === null) {
+                    // WebXR's requestReferenceSpace only works if the corresponding feature
+                    // was requested at session creation time. For simplicity, just ask for
+                    // the interesting ones as optional features, but be aware that the
+                    // requestReferenceSpace call will fail if it turns out to be unavailable.
+                    // ('local' is always available for immersive sessions and doesn't need to
+                    // be requested separately.)
+
+                    const sessionInit = { optionalFeatures: ['local-floor', 'bounded-floor', 'hand-tracking', 'layers'] };
+                    navigator.xr.requestSession('immersive-vr', sessionInit).then(onSessionStarted);
+                } else {
+                    currentSession.end();
+                }
+            };
+        }
+
+        const disableButton = () => {
+            button.style.display = '';
+
+            button.style.cursor = 'auto';
+            button.style.left = 'calc(50% - 75px)';
+            button.style.width = '150px';
+
+            button.onmouseenter = null;
+            button.onmouseleave = null;
+
+            button.onclick = null;
+        }
+
+        const showWebXRNotFound = () => {
+            disableButton();
+            button.textContent = 'VR Device Not Found';
+        }
+
+        const showVRNotAllowed = (exception) => {
+            disableButton();
+            console.warn('Exception when trying to call xr.isSessionSupported', exception);
+            button.textContent = 'VR Not Allowed';
+        }
+
+        const stylizeElement = (element) => {
+            element.style.position = 'absolute';
+            element.style.bottom = '12px';
+            element.style.padding = '12px 6px';
+            element.style.border = '1px solid rgba(255, 255, 255, 0.6)';
+            element.style.borderRadius = '6px';
+            element.style.background = '#d97706';
+            element.style.color = '#fff';
+            element.style.fontSize = '18px';
+            element.style.fontWeight = '700';
+            element.style.fontFamily = 'lato, sans-serif';
+            element.style.textAlign = 'center';
+            element.style.outline = 'none';
+            element.style.zIndex = '999';
+        }
+
+        if ('xr' in navigator) {
+            button.id = 'VRButton';
+            button.style.display = 'none';
+
+            stylizeElement(button);
+
+            navigator.xr.isSessionSupported('immersive-vr').then((supported) => {
+                supported ? showEnterVR() : showWebXRNotFound();
+
+                if (supported && VRButton.xrSessionIsGranted) {
+                    button.click();
+                }
+
+            }).catch(showVRNotAllowed);
+
+            return button;
+
+        } else {
+            const message = document.createElement('a');
+
+            if (window.isSecureContext === false) {
+                message.href = document.location.href.replace(/^http:/, 'https:');
+                message.innerHTML = 'VR Mode Requires HTTPS';
+            } else {
+                message.href = '#unavailable';
+                message.innerHTML = 'VR Mode Unavailable (?)';
+            }
+
+            stylizeElement(message);
+
+            message.style.left = 'calc(50% - 120px)';
+            message.style.width = '240px';
+            message.style.textDecoration = 'none';
+            message.style.opacity = "0.7";
+            message.style.background = '#a3a3a3';
+            message.style.border = '1px solid #a3a3a3';
+
+            return message;
+        }
+    }
+
+    static registerSessionGrantedListener() {
+        if ('xr' in navigator) {
+            // WebXRViewer (based on Firefox) has a bug where addEventListener
+            // throws a silent exception and aborts execution entirely.
+            if (/WebXRViewer\//i.test(navigator.userAgent)) return;
+
+            navigator.xr.addEventListener('sessiongranted', () => {
+                VRButton.xrSessionIsGranted = true;
+            });
+        }
+    }
+}
+
+VRButton.xrSessionIsGranted = false;
+VRButton.registerSessionGrantedListener();
+
+export { VRButton };
